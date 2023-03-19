3ee04aef48d886175da291134e31c8e7bfa4d967
lars-reimann <lars-reimann@users.noreply.github.com>
2023-03-19 21:38:07 +0000

    style: apply automated linter fixes

--- tests/library_analyzer/cli/test_cli.py
+++ tests/library_analyzer/cli/test_cli.py
@@ -3,37 +3,93 @@ import subprocess
 
 def test_cli_api() -> None:
     subprocess.run(
-        ["poetry", "run", "analyze-library", "api", "-p", "library_analyzer", "-s", "src", "-o", "out"],
-        check=True
+        [
+            "poetry",
+            "run",
+            "analyze-library",
+            "api",
+            "-p",
+            "library_analyzer",
+            "-s",
+            "src",
+            "-o",
+            "out",
+        ],
+        check=True,
     )
 
 
 def test_cli_usages() -> None:
     subprocess.run(
-        ["poetry", "run", "analyze-library", "usages", "-p", "library_analyzer", "-c", "library_analyzer", "-o", "out"],
-        check=True
+        [
+            "poetry",
+            "run",
+            "analyze-library",
+            "usages",
+            "-p",
+            "library_analyzer",
+            "-c",
+            "library_analyzer",
+            "-o",
+            "out",
+        ],
+        check=True,
     )
 
 
 def test_cli_annotations() -> None:
     subprocess.run(
-        ["poetry", "run", "analyze-library", "annotations", "-a", "tests/data/removeAnnotations/api_data.json", "-u",
-         "tests/data/removeAnnotations/usage_data.json", "-o", "out/annotations.json"],
-        check=True
+        [
+            "poetry",
+            "run",
+            "analyze-library",
+            "annotations",
+            "-a",
+            "tests/data/removeAnnotations/api_data.json",
+            "-u",
+            "tests/data/removeAnnotations/usage_data.json",
+            "-o",
+            "out/annotations.json",
+        ],
+        check=True,
     )
 
 
 def test_cli_all() -> None:
     subprocess.run(
-        ["poetry", "run", "analyze-library", "all", "-p", "library_analyzer", "-s", "src", "-c",
-         "library_analyzer", "-o", "out"],
-        check=True
+        [
+            "poetry",
+            "run",
+            "analyze-library",
+            "all",
+            "-p",
+            "library_analyzer",
+            "-s",
+            "src",
+            "-c",
+            "library_analyzer",
+            "-o",
+            "out",
+        ],
+        check=True,
     )
 
 
 def test_cli_migration() -> None:
     subprocess.run(
-        ["poetry", "run", "analyze-library", "migrate", "-a1", "tests/data/migration/apiv1_data.json", "-a2",
-         "tests/data/migration/apiv2_data.json", "-a", "tests/data/migration/annotationv1.json", "-o", "out"],
-        check=True
+        [
+            "poetry",
+            "run",
+            "analyze-library",
+            "migrate",
+            "-a1",
+            "tests/data/migration/apiv1_data.json",
+            "-a2",
+            "tests/data/migration/apiv2_data.json",
+            "-a",
+            "tests/data/migration/annotationv1.json",
+            "-o",
+            "out",
+        ],
+        check=True,
     )
