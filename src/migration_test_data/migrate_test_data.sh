5336ab19d0838fdb0a6fa1407790eee5691977c4
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-03-20 14:59:11 +0100

    update migration scripts for new repo

--- src/migration_test_data/migrate_test_data.sh
+++ src/migration_test_data/migrate_test_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ./migration_test_data/api/apiv2/ || exit
+cd ./src/migration_test_data/api/apiv2/ || exit
 mv library_apiv2 library_api
 cd ../../..
-poetry run python -m library_analyzer api -s ./migration_test_data/api/apiv2/library_api -p library_api -o ./migration_test_data/api/apiv2
-poetry run python -m library_analyzer migrate -a1 ./migration_test_data/api/apiv1/library_api__api.json -a2 ./migration_test_data/api/apiv2/library_api__api.json -a ./migration_test_data/api/apiv1/annotations_apiv1.json -o ./migration_test_data
-cd ./migration_test_data/api/apiv2/ || exit
+poetry run python -m library_analyzer api -s ./src/migration_test_data/api/apiv2/library_api -p library_api -o ./src/migration_test_data/api/apiv2
+poetry run python -m library_analyzer migrate -a1 ./src/migration_test_data/api/apiv1/library_api__api.json -a2 ./src/migration_test_data/api/apiv2/library_api__api.json -a ./src/migration_test_data/api/apiv1/annotations_apiv1.json -o ./migration_test_data
+cd ./src/migration_test_data/api/apiv2/ || exit
 mv library_api library_apiv2
