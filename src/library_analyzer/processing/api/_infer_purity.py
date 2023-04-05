153166cb2c9a70c0f8f1769899ba9657e76b7b90
lukarade <84092952+lukarade@users.noreply.github.com>
2023-04-05 02:01:33 +0200

    feat: (WIP) added prototype analysis for ClassDef

--- src/library_analyzer/processing/api/_infer_purity.py
+++ src/library_analyzer/processing/api/_infer_purity.py
@@ -119,7 +119,7 @@ class ParameterUsageHandler:
             # print("PARAMETER IS USED", variable)
         pass
 
-    def enter_call(self, node: astroid.Call):
+    def enter_call(self, node: astroid.Call) -> None:
         arguments = node.args
         if isinstance(node.func, astroid.Attribute):
             function_name = node.func.attrname
@@ -151,6 +151,8 @@ class PurityHandler:
     def enter_functiondef(self, node: astroid.FunctionDef) -> None:
         # print(f"Enter functionDef node: {node.as_string()}")
         # Handle the FunctionDef node here
+
+        """Handle Parameters, which are actually used in the function"""
         if node.args.args:
             parameters = []
             for arg in node.args.args:
@@ -162,13 +164,19 @@ class PurityHandler:
             visitor.walk(node)
             # print("Used parameters: ", parameter_handler.get_used_parameters())
             for parameter in parameter_handler.get_used_parameters():
-
                 self.append_reason([Call(ParameterAccess(parameter=parameter.parameter,
                                                          function=parameter.call))])
+
         for nodes in node.body:
+            """Handle Global Variables"""
+            # TODO: Check if a global variable is used in the function
             if isinstance(nodes, astroid.Global):
                 self.append_reason([VariableWrite(GlobalAccess(name=nodes.names[0], module=node.root().name))])
                 print("GLOBAL", nodes.names[0], node.root().name)
+            # TODO: Check if a global variable is written in the function
+
+            # TODO: Handle Built-In Functions
+            """Handle Built-In Functions"""
 
     def enter_assign(self, node: astroid.Assign) -> None:
         # print(f"Entering Assign node {node}, {node.as_string()}")
@@ -257,6 +265,37 @@ class PurityHandler:
     def enter_with(self, node: astroid.With) -> None:
         # print(f"Entering With node {node.as_string()}")
         # Handle the With node here
+        pass
+
+    def enter_module(self, node: astroid.Module) -> None:
+        # print(f"Entering Module node {node.as_string()}")
+        # Handle the Module node here
+        # print(node.globals)
+        pass
+
+    def enter_classdef(self, node: astroid.ClassDef) -> None:
+        # print(f"Entering ClassDef node")
+        # print(f"Entering ClassDef node {node.as_string()}")
+        result = []
+        class_function_list = []
+
+        for child in node.get_children():
+            if isinstance(child, astroid.FunctionDef):
+                class_function_list.append(child.name)
+                print(f"Function {child.name} is defined in {node.name}\n")
+
+        for child in node.get_children():
+            inner_class_purity_handler = PurityHandler()
+            inner_class_walker = ASTWalker(inner_class_purity_handler)
+            inner_class_walker.walk(child)
+
+            purity_result = determine_purity(inner_class_purity_handler.purity_reason)
+            result.append(generate_purity_information(child, purity_result))
+            inner_class_purity_handler.purity_reason = []
+
+
+        print(f"Class {node.name} is {result}")
+        print_result_list(result)
         pass
 
 
@@ -353,12 +392,13 @@ def infer_purity(code: str) -> list[PurityInformation]:
     purity_handler: PurityHandler = PurityHandler()
     walker = ASTWalker(purity_handler)
     result = []
-    for node in module.body:
-        tree = astroid.extract_node(node.as_string())
-        print(tree.repr_tree())
-        print("\n\n")
+    # for node in module.body:
+    #     tree = astroid.extract_node(node.as_string())
+    #     print(tree.repr_tree())
+    #     print("\n\n")
 
     for node in module.body:
+        # print(node.as_string())
         walker.walk(node)
         purity_result = determine_purity(purity_handler.purity_reason)
         result.append(generate_purity_information(node, purity_result))
@@ -391,24 +431,33 @@ def extract_impurity_reasons(purity: PurityResult) -> list[ImpurityIndicator]:
     return purity.reasons
 
 
-def generate_purity_information(function: astroid.FunctionDef, purity_result: PurityResult) -> PurityInformation:
+def generate_purity_information(function: astroid.NodeNG, purity_result: PurityResult) -> PurityInformation:
     function_id = calc_function_id(function)
     reasons = extract_impurity_reasons(purity_result)
     purity_info = PurityInformation(function_id, reasons)
     return purity_info
 
 
+def get_module_code(module: str) -> str:
+    if module.endswith(".py"):
+        module = module[:-3]
+    module = module.replace(".", "/")
+    with open(f"{module}.py") as file:
+        return file.read()
+    # TODO: remove conversion to string and use astroid nodes instead
+
 def calc_function_id(node: astroid.NodeNG) -> FunctionID | None:
-    if not isinstance(node, astroid.FunctionDef):
-        return FunctionID("NODE (not a functionDef):", node.as_string(), 0, 0, node.__class__.__name__)
-    # module = node.root().name   TODO: Use module correctly
+    module = node.parent.name  # TODO: This is not correct, it should be the module name
     # module = "_infer_purity.py"
-    # if module.endswith(".py"):
-    #    module = module[:-3]
+    if module.endswith(".py"):
+       module = module[:-3]
+    if not isinstance(node, astroid.FunctionDef):
+        return FunctionID(module, node.as_string().split('=')[0].strip(), 0, 0, node.__class__.__name__)
+
     name = node.name
     line = node.position.lineno
     col = node.position.col_offset
-    return FunctionID("NODE:", name, line, col, astroid.FunctionDef)
+    return FunctionID(module, name, line, col, node.__class__.__name__)
 # TODO: This function should return a correct FunctionID object for a given function and an other ID for everything else
 
 
@@ -425,7 +474,7 @@ def remove_irrelevant_information(purity_information: list[PurityInformation]) -
     return result
 
 
-# this function is only for visualization purposes
+# this function are only for visualization purposes
 def get_purity_result_str(indicators: list[ImpurityIndicator]) -> str:
     if len(indicators) == 0:
         return "Pure"
@@ -435,44 +484,58 @@ def get_purity_result_str(indicators: list[ImpurityIndicator]) -> str:
     return "Unknown"
 
 
+def print_result_list(result: list[PurityInformation]):
+    for purity_info in result:
+        p = get_purity_result_str(purity_info.reasons)
+        print(f"Module:{purity_info.id.module}, Name: {purity_info.id.name} of type {purity_info.id.node_type} is {p} because {purity_info.reasons} \n")
+
+
 if __name__ == "__main__":
-    # import astroid  # impure import
+    x = get_module_code("C:/Users/Lukas Radermacher/Documents/GitHub/Library-Analyzer/src/library_analyzer/utils/_ASTWalker.py")
+    # print(x)
+    result = infer_purity(x)
+    print_result_list(result)
+
     sourcecode = """
-
-       def impure_fun(a):
-           impure_call(a) # call => impure
-           impure_call(a) # call => impure - check if the analysis is correct for multiple calls - done
-           return a
-
-       def pure_fun(a):
-           a += 1
-           return a
-
        class A:
            def __init__(self):
                self.number = 42
+
+           def __eq__(self, other):
+               return self.number == other.number
+
+           def __hash__(self):
+               return hash(self.number)
+
+           def function_access(self):
+               g() # Call => impure
+
        def instance_access():
            a = A()
            return a.number # InstanceAccess => impure
 
-       class B:
-           name = "test"
-       b = B()
-
-       def attribute_access():
-           res = b.name # AttributeAccess => impure
-           return res
-
-       glob = g(1)
-
-       def fun(a):
-           h(a)
-           b =  g(a) # call => impure
-           b += 1
-           return b
-
        """
+
+    # this function is intended to be used for checking if there are any global variables in the file
+    def get_global_variables_from_file(file_path: str) -> list[str]:
+        with open(file_path, 'r') as f:
+            module = astroid.parse(f.read())
+
+        global_vars = []
+        for node in module.body:  # TODO: improve this to make it faster by specifying the depth of the search
+            if isinstance(node, astroid.Assign):
+                for target in node.targets:
+                    if isinstance(target, astroid.AssignName) and target.name not in global_vars:
+                        global_vars.append(target.name)
+
+        return global_vars
+
+
+    # print(get_global_variables_from_file("C:/Users/Lukas Radermacher/Documents/GitHub/Library-Analyzer/src/"
+    #                                      "library_analyzer/processing/api/model/_purity.py"))
+    # print(get_global_variables_from_file("C:/Users/Lukas Radermacher/Documents/GitHub/Library-Analyzer/src/library_analyzer/processing/api/_infer_purity.py"))
+
     result_list = infer_purity(sourcecode)
-    for pinfo in result_list:
-        p = get_purity_result_str(pinfo.reasons)
-        print(f"{pinfo.id.module} {pinfo.id.name} is {p} because {pinfo.reasons} \n")
+    print_result_list(result_list)
+    # for i in dir(astroid):
+    #     print(i)
