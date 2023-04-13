d91b536a10697c7400bacbf7befdb258253af3d1
lukarade <84092952+lukarade@users.noreply.github.com>
2023-04-13 02:11:22 +0200

    feat: resolve_reference for target and value name nodes

--- src/library_analyzer/processing/api/_resolve_references.py
+++ src/library_analyzer/processing/api/_resolve_references.py
@@ -3,8 +3,6 @@ from enum import Enum, auto
 from typing import List
 
 import astroid
-from astroid import Name
-from astroid.helpers import safe_infer
 
 from library_analyzer.utils import ASTWalker
 
@@ -21,19 +19,19 @@ class Reference:
     # scope: astroid.Module | astroid.FunctionDef | astroid.ClassDef  # TODO: implement scope
     usage: Usage
     potential_references: List[astroid.Name | astroid.AssignName] = field(default_factory=list)
-    list_is_complete: bool = False  # if True, then the list of potential references is completed
+    list_is_complete: bool = False  # if True, then the list potential_references is completed
     # TODO: implement a methode to check if the list is complete: all references are found
     #  the list is only completed if every reference is found
 
 
 @dataclass
-class ReferenceHandler:
+class NameNodeFinder:
     # search_name_in_nodes: list[astroid.Assign]
     names_list: list[astroid.Name | astroid.AssignName] = field(default_factory=list)
 
     # AssignName is used to find the name if it is used as a value in an assignment
     def enter_name(self, node: astroid.Name) -> None:
-        if isinstance(node.parent, astroid.Assign | astroid.AssignAttr | astroid.Attribute | astroid.AugAssign | astroid.Return | astroid.Compare | astroid.For):
+        if isinstance(node.parent, astroid.Assign | astroid.AssignAttr | astroid.Attribute | astroid.AugAssign | astroid.Return | astroid.Compare | astroid.For | astroid.BinOp | astroid.BoolOp):
             self.names_list.append(node)
         if isinstance(node.parent, astroid.Call):
             if isinstance(node.parent.func, astroid.Name):
@@ -43,27 +41,27 @@ class ReferenceHandler:
 
     # AssignName is used to find the name if it is used as a target in an assignment
     def enter_assignname(self, node: astroid.AssignName) -> None:
-        if isinstance(node.parent, astroid.Assign | astroid.Arguments | astroid.AssignAttr | astroid.Attribute| astroid.AugAssign | astroid.AnnAssign | astroid.Return | astroid.Compare | astroid.For):
+        if isinstance(node.parent, astroid.Assign | astroid.Arguments | astroid.AssignAttr | astroid.Attribute | astroid.AugAssign | astroid.AnnAssign | astroid.Return | astroid.Compare | astroid.For):
             self.names_list.append(node)
 
     # We do not need AugAssign, since it uses AssignName as a target and Name as value
 
 
 def get_name_nodes(module: astroid.NodeNG) -> list[list[astroid.Name]]:
-    reference_handler = ReferenceHandler()
-    walker = ASTWalker(reference_handler)
+    name_node_handler = NameNodeFinder()
+    walker = ASTWalker(name_node_handler)
     name_nodes: list[list[astroid.Name]] = []
 
     if isinstance(module, astroid.Module):
         for node in module.body:
             if isinstance(node, astroid.FunctionDef):  # filter all function definitions
                 walker.walk(node)
-                name_nodes.append(reference_handler.names_list)
-                reference_handler.names_list = []
+                name_nodes.append(name_node_handler.names_list)
+                name_node_handler.names_list = []
             if isinstance(node, astroid.ClassDef):  # filter all class definitions
                 walker.walk(node)
-                name_nodes.append(reference_handler.names_list)
-                reference_handler.names_list = []
+                name_nodes.append(name_node_handler.names_list)
+                name_node_handler.names_list = []
     # for i in name_nodes:
     #    print(i)
 
@@ -75,25 +73,70 @@ def construct_reference_list(names_list: list[astroid.Name | astroid.AssignName]
     references_without_potential_references: list[Reference] = []
     for name in names_list:
         if isinstance(name, astroid.Name):
-            references_without_potential_references.append(Reference(name, Usage.TARGET, [], False))
-        if isinstance(name, astroid.AssignName):
             references_without_potential_references.append(Reference(name, Usage.VALUE, [], False))
+        if isinstance(name, astroid.AssignName):
+            references_without_potential_references.append(Reference(name, Usage.TARGET, [], False))
 
     return references_without_potential_references
 
 
-def add_potential_references(references_without_potential_references: Reference) -> Reference:
-    """Add all potential references to a reference."""
-    pass
+def add_potential_value_references(reference: Reference, reference_list: list[Reference]) -> Reference:
+    """Add all potential value references to a reference.
+
+    A potential value reference is a reference where the name is used as a value.
+    Therefor we need to check all nodes further down the list where the name is used as a value.
+    """
+    references_complete = reference
+    # check all nodes further down the list where the name is used as a value
+    if reference in reference_list:
+        for reference_next in reference_list[reference_list.index(reference):]:
+            if reference_next.name.name == reference.name.name:
+                if reference_next.usage.name == "VALUE":
+                    references_complete.potential_references.append(reference_next.name)
+
+    # TODO: check if the list is actually complete
+    references_complete.list_is_complete = True
+
+    return references_complete
 
 
-def resolve_references(module_names: list[astroid.Name]) -> None:
-    """Resolve references in a node."""
+def add_potential_target_references(reference: Reference, reference_list: list[Reference]) -> Reference:
+    """Add all potential target references to a reference.
+
+    A potential target reference is a reference where the name is used as a target.
+    Therefor we need to check all nodes further up the list where the name is used as a target.
+    """
+    references_complete = reference
+    # check all nodes further down the list where the name is used as a target
+    if reference in reference_list:
+        for reference_next in reference_list[:reference_list.index(reference)]:
+            if reference_next.name.name == reference.name.name:
+                if reference_next.usage.name == "TARGET":
+                    references_complete.potential_references.append(reference_next.name)
+
+    references_complete.list_is_complete = True
+
+    return references_complete
+
+
+def resolve_references(module_names: list[astroid.Name]) -> list[Reference]:
+    """Resolve references in a node.
+
+    The following methods are called:
+    * construct_reference_list: construct a list of references from a list of name nodes but without potential references
+    * add_potential_value_references: add all potential value references to a reference
+    * add_potential_target_references: add all potential target references to a reference
+    """
+
     reference_list_complete: list[Reference] = []
     reference_list_proto = construct_reference_list(module_names)
     for reference in reference_list_proto:
-        reference_complete = add_potential_references(reference)
-        reference_list_complete.append(reference_complete)
+        if reference.usage.name == "TARGET":
+            reference_complete = add_potential_value_references(reference, reference_list_proto)
+            reference_list_complete.append(reference_complete)
+        if reference.usage.name == "VALUE":
+            reference_complete = add_potential_target_references(reference, reference_list_proto)
+            reference_list_complete.append(reference_complete)
 
         # TODO: since we have found all name Nodes, we need to find the scope of the current name node
         #  and then search for all name nodes in that scope where the name is used
@@ -101,4 +144,4 @@ def resolve_references(module_names: list[astroid.Name]) -> None:
         #  check all nodes further down the list where the name is used as a target
         #  if the name is used as a target in an assignment, then we need to find the value of the assignment?
 
-    pass
+    return reference_list_complete
--- tests/library_analyzer/processing/api/test_resolve_references.py
+++ tests/library_analyzer/processing/api/test_resolve_references.py
@@ -29,9 +29,7 @@ from library_analyzer.processing.api import (
                 def glob():
                     global glob1
             """,
-            # TODO: "global glob1" is not a Name node - what should we do, since it is never used?
-            #  see next case:
-            ["TODO"]
+            []
         ),
         (
             """
@@ -50,10 +48,12 @@ from library_analyzer.processing.api import (
         ),  # TODO: Problem with instance attributes since: A.class_attr1 is not a Name node
         (
             """
+
                 def instance_attr():
-                    var1 = B().instance_attr
+                    b = B()
+                    var1 = b.instance_attr
             """,
-            ['AssignName.var1', 'Name.b.instance_attr']  # TODO: how do we need B().instance_attr1?
+            ['AssignName.b', 'AssignName.var1', 'Name.b.instance_attr']  # TODO: how do we need B().instance_attr1?
         ),  # TODO: Problem with instance attributes since: B().instance_attr1 is not a Name node
         (
             """
@@ -106,9 +106,9 @@ from library_analyzer.processing.api import (
         (
             """
                 def if_elif_state():
-                    if var1 > 0:
+                    if var1 & True:
                         do_something()
-                    elif var1 > var2:
+                    elif var1 | var2:
                         do_something_else()
             """, ['Name.var1', 'Name.var1', 'Name.var2']
         ),
@@ -126,15 +126,91 @@ from library_analyzer.processing.api import (
         ),
         (
             """
+                def func_call_par(param):
+                    var1 = param + func(param)
+            """, ['AssignName.param', 'AssignName.var1', 'Name.param', 'Name.param']
+        ),
+        (
+            """
+                def bin_op():
+                    var1 = 20 + var2
+            """, ['AssignName.var1', 'Name.var2']
+        ),
+        (
+            """
+                def bool_op():
+                    var1 = True and var2
+            """, ['AssignName.var1', 'Name.var2']
+        ),
+        (
+            """
                 def unresolved_reference():
-                    var1 =
+                    var1 = x
             """, ['AssignName.var1']
-        )  # TODO: this should better be checked in the resolve_references test?
+        ),  # TODO: this should better be checked in the resolve_references test?
+        (
+            """
+                from collections.abc import Callable
+                from typing import Any
+
+                import astroid
+
+                _EnterAndLeaveFunctions = tuple[
+                    Callable[[astroid.NodeNG], None] | None,
+                    Callable[[astroid.NodeNG], None] | None,
+                ]
+
+
+                class ASTWalker:
+                    def __init__(self, handler: Any) -> None:
+                        self._handler = handler
+                        self._cache: dict[type, _EnterAndLeaveFunctions] = {}
+
+                    def walk(self, node: astroid.NodeNG) -> None:
+                        self.__walk(node, set())
+
+                    def __walk(self, node: astroid.NodeNG, visited_nodes: set[astroid.NodeNG]) -> None:
+                        if node in visited_nodes:
+                            raise AssertionError("Node visited twice")
+                        visited_nodes.add(node)
+
+                        self.__enter(node)
+                        for child_node in node.get_children():
+                            self.__walk(child_node, visited_nodes)
+                        self.__leave(node)
+
+                    def __enter(self, node: astroid.NodeNG) -> None:
+                        method = self.__get_callbacks(node)[0]
+                        if method is not None:
+                            method(node)
+
+                    def __leave(self, node: astroid.NodeNG) -> None:
+                        method = self.__get_callbacks(node)[1]
+                        if method is not None:
+                            method(node)
+
+                    def __get_callbacks(self, node: astroid.NodeNG) -> _EnterAndLeaveFunctions:
+                        klass = node.__class__
+                        methods = self._cache.get(klass)
+
+                        if methods is None:
+                            handler = self._handler
+                            class_name = klass.__name__.lower()
+                            enter_method = getattr(handler, f"enter_{class_name}", getattr(handler, "enter_default", None))
+                            leave_method = getattr(handler, f"leave_{class_name}", getattr(handler, "leave_default", None))
+                            self._cache[klass] = (enter_method, leave_method)
+                        else:
+                            enter_method, leave_method = methods
+
+                        return enter_method, leave_method
+
+            """, []
+        )
     ],
     ids=[
         "Assign",
         "Assign Parameter",
-        "Global",
+        "Global unused",
         "Global and Assign",
         "Assign Class Attribute",
         "Assign Instance Attribute",
@@ -148,7 +224,11 @@ from library_analyzer.processing.api import (
         "If Elif",
         "AnnAssign",
         "FuncCall",
-        "Unresolved Reference"
+        "FuncCall Parameter",
+        "BinOp",
+        "BoolOp",
+        "Unresolved Reference",
+        "ASTWalker"
     ]
 )
 def test_get_name_nodes(code: str, expected: str) -> None:
@@ -181,9 +261,10 @@ def transform_actual_names(names):
         (
             """
                 def local_parameter(a):
-                    var1 = a
-                    res = var1
-                    return res
+                    var1 = 2 * a
+                    return var1
+
+                local_parameter(10)
             """,
             []
         ),        (
@@ -193,19 +274,17 @@ def transform_actual_names(names):
                     global glob1
 
                     var1 = glob1
-                    res = var1
-                    return res
+                    return var1
             """,
             []
         ),        (
             """
                 class A:
-                    class_attr1 = 10
+                    class_attr1 = 20
 
                 def local_class_attr():
                     var1 = A.class_attr1
-                    res = var1
-                    return res
+                    return var1
             """,
             []
         ),
@@ -217,8 +296,7 @@ def transform_actual_names(names):
 
                 def local_instance_attr():
                     var1 = B().instance_attr1
-                    res = var1
-                    return res
+                    return var1
             """,
             []
         ),
@@ -240,77 +318,79 @@ def test_resolve_references_local(code, expected):
     for module_name in all_names_list:
         resolved_references.append(resolve_references(module_name))
 
-    print(resolved_references)
+    for resolved in resolved_references:
+        print(resolved, "\n")
+        assert resolved == expected
 
 
-@pytest.mark.parametrize(
-    ("code", "expected"),
-    [
-        (
-            """
-                def local_const():
-                    var1 = 20
-                    res = var1
-                    return res
-            """,
-            []
-        ),
-        (
-            """
-                def local_parameter(a):
-                    var1 = a
-                    res = var1
-                    return res
-            """,
-            []
-        ),        (
-            """
-                glob1 = 10
-                def local_global():
-                    global glob1
-
-                    var1 = glob1
-                    res = var1
-                    return res
-            """,
-            []
-        ),        (
-            """
-                class A:
-                    class_attr1 = 10
-
-                def local_class_attr():
-                    var1 = A.class_attr1
-                    res = var1
-                    return res
-            """,
-            []
-        ),
-        (
-            """
-                class B:
-                    def __init__(self):
-                        self.instance_attr1 = 10
-
-                def local_instance_attr():
-                    var1 = B().instance_attr1
-                    res = var1
-                    return res
-            """,
-            []
-        ),
-    ],
-    ids=[
-        "constant as local variable",
-        "parameter as local variable",
-        "global as local variable",
-        "class attribute as local variable",
-        "instance attribute as local variable",
-    ]
-)
-def test_resolve_references_global(code, expected):
-    module = astroid.parse(code)
-    print(module.repr_tree(), "\n")
-    names_list = get_name_nodes(module)
-    for node in names_list:
-        resolve_references(node)
+# @pytest.mark.parametrize(
+#     ("code", "expected"),
+#     [
+#         (
+#             """
+#                 def local_const():
+#                     var1 = 20
+#                     res = var1
+#                     return res
+#             """,
+#             []
+#         ),
+#         (
+#             """
+#                 def local_parameter(a):
+#                     var1 = a
+#                     res = var1
+#                     return res
+#             """,
+#             []
+#         ),        (
+#             """
+#                 glob1 = 10
+#                 def local_global():
+#                     global glob1
+#
+#                     var1 = glob1
+#                     res = var1
+#                     return res
+#             """,
+#             []
+#         ),        (
+#             """
+#                 class A:
+#                     class_attr1 = 10
+#
+#                 def local_class_attr():
+#                     var1 = A.class_attr1
+#                     res = var1
+#                     return res
+#             """,
+#             []
+#         ),
+#         (
+#             """
+#                 class B:
+#                     def __init__(self):
+#                         self.instance_attr1 = 10
+#
+#                 def local_instance_attr():
+#                     var1 = B().instance_attr1
+#                     res = var1
+#                     return res
+#             """,
+#             []
+#         ),
+#     ],
+#     ids=[
+#         "constant as local variable",
+#         "parameter as local variable",
+#         "global as local variable",
+#         "class attribute as local variable",
+#         "instance attribute as local variable",
+#     ]
+# )
+# def test_resolve_references_global(code, expected):
+#     module = astroid.parse(code)
+#     print(module.repr_tree(), "\n")
+#     names_list = get_name_nodes(module)
+#     for node in names_list:
+#         resolve_references(node)
