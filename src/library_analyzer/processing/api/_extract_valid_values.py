daa3bc997348e45c76b3b9570d1e728521e4da82
nvollroth <n.vollroth@live.de>
2023-05-04 10:56:38 +0200

    "Replaced" regex with spaCy-Rules. (Currently, the old part is  not removed)
    Adjusted some test cases

--- src/library_analyzer/processing/api/_extract_valid_values.py
+++ src/library_analyzer/processing/api/_extract_valid_values.py
@@ -1,5 +1,11 @@
 import re
 from dataclasses import dataclass, field
+from typing import Any
+
+import spacy
+from spacy import Language
+from spacy.matcher import Matcher
+from spacy.tokens import Doc, Span
 
 
 @dataclass
@@ -201,3 +207,217 @@ def _extract_from_description_when_set_to(description: str) -> set[str]:
     pattern = r"When set to (\"[^\"]*\"|'[^']*'|None|True|False)"
     matches = re.findall(pattern, description, re.IGNORECASE)
     return set(matches)
+
+
+
+
+
+
+
+_enum_valid_values_are = [
+    {"LOWER": "valid"},
+    {"LOWER": "values"},
+    {"LOWER": "are"}
+]
+
+_enum_when_set_to = [
+    {"LOWER": "when"},
+    {"LOWER": "set"},
+    {"LOWER": "to"}
+]
+
+_enum_indented_if_listing = [
+    {"LOWER": "if"},
+    {"ORTH": {"IN": ["'", '"', "True", "False", "None"]}},
+    {}
+]
+
+_enum_type_curly = [
+    {"ORTH": "{"},
+    {"IS_ASCII": True, "OP": "+"},
+    {"ORTH": "}"}
+]
+
+_enum_str = [
+    {"LOWER": "str"}
+]
+
+_enum_single_val_bool_none = [
+    {"ORTH": {"IN": ["True", "False", "None"]}},
+    {"ORTH": ":"}
+]
+
+_enum_single_val_quoted = [
+    {"ORTH": {"IN": ["'", '"']}},
+    {"OP": "+"},
+    {"ORTH": {"IN": ["'", '"']}},
+    {"ORTH": ":"}
+]
+
+
+@dataclass
+class MatcherConfiguration:
+    _nlp: Language = None
+    _matcher: Matcher = None
+
+    # Rules to be checked
+    when_set_to: bool = True
+    valid_values_are: bool = True
+    type_curly: bool = True
+    indented_if_listings: bool = True
+    single_vals: bool = True
+
+    def __post_init__(self) -> None:
+        self._nlp = spacy.load("en_core_web_sm")
+        self._matcher = Matcher(self._nlp.vocab)
+
+        self._matcher.add("ENUM_STR", [_enum_str])
+
+        if self.when_set_to:
+            self._matcher.add("ENUM_WHEN_SET_TO", [_enum_when_set_to], on_match=_extract_single_value)
+        if self.valid_values_are:
+            self._matcher.add("ENUM_VALID_VALUES_ARE", [_enum_valid_values_are], on_match=_extract_list)
+        if self.type_curly:
+            self._matcher.add("ENUM_TYPE_CURLY", [_enum_type_curly], on_match=_extract_list)
+        if self.indented_if_listings:
+            self._matcher.add("ENUM_INDENTED_IF_LISTING", [_enum_indented_if_listing], on_match=_extract_indented_value)
+        if self.single_vals:
+            self._matcher.add("ENUM_SINGLE_VALS", [_enum_single_val_quoted, _enum_single_val_bool_none],
+                              on_match=_extract_indented_single_value)
+
+    def get_matcher(self) -> Matcher:
+        return self._matcher
+
+    def get_nlp(self) -> Language:
+        return self._nlp
+
+
+_extracted = []
+
+
+def _extract_list(
+    nlp_matcher: Matcher,  # noqa : ARG001
+    doc: Doc,
+    i: int,  # noqa : ARG001
+    nlp_matches: list[tuple[Any, ...]]  # noqa : ARG001
+) -> Any | None:
+    found_list = False
+    found_minus = False
+    ex = []
+
+    for token in doc:
+        if token.text in ["[", "{"]:
+            found_list = True
+        elif token.text in ["]", "}"]:
+            break
+
+        if found_list and token.text not in ["'", '"', ",", "[", "{", "}"]:
+            if token.text in ["True", "False"]:
+                ex.append("True")
+                ex.append("False")
+            else:
+                if token.text in ["-", "_", " "]:
+                    found_minus = True
+                    ex[len(ex) - 1] += token.text
+                    continue
+                elif found_minus:
+                    ex[len(ex) - 1] += token.text
+                    found_minus = False
+                    continue
+
+                ex.append(token.text)
+
+    ex = ['"' + x + '"' for x in ex]
+    _extracted.extend(ex)
+
+    return None
+
+
+def _extract_single_value(
+    nlp_matcher: Matcher,  # noqa : ARG001
+    doc: Doc, i: int,
+    nlp_matches: list[tuple[Any, ...]]
+) -> Any | None:
+    _, _, end = nlp_matches[i]
+    value = '"' + doc[end + 1].text + '"'
+    _extracted.append(value)
+
+    return None
+
+def _extract_indented_single_value(
+    nlp_matcher: Matcher,  # noqa : ARG001
+    doc: Doc,
+    i: int,
+    nlp_matches: list[tuple[Any, ...]]
+) -> Any | None:
+
+    _, start, end = nlp_matches[i]
+    value = doc[start:end-1]
+    value = value.text
+
+    if value[0] in ["'", '"']:
+        value = value.replace("'", '"')
+
+    _extracted.append(value)
+
+    return None
+
+
+
+def _extract_indented_value(nlp_matcher: Matcher, doc: Doc, i: int, nlp_matches: list[tuple[Any, ...]]) -> Any | None:
+    _, start, end = nlp_matches[i]
+    # text_ = doc[start:end].text.strip()
+
+    span_ = doc[start:end]
+    found_quotes = False
+
+    for token in span_:
+        if token.text == "None":
+            _extracted.append("None")
+        elif token.text in ["True", "False"]:
+            _extracted.append("True")
+            _extracted.append("False")
+        elif token.text in ["'", '"']:
+            found_quotes = not found_quotes
+        elif found_quotes:
+            _extracted.append('"' + token.text + '"')
+
+    return None
+
+
+def _nlp_matches_to_readable_matches(
+    nlp_matches: list[tuple[int, int, int]], nlp_: Language, doc_: Doc
+) -> list[tuple[str, Span]]:
+    return [(nlp_.vocab.strings[match_id], doc_[start:end]) for match_id, start, end in nlp_matches]
+
+
+
+
+def extract(description: str, type_string: str) -> set[str]:
+    _extracted.clear()
+
+    nlp = MATCHER_CONFIG.get_nlp()
+    matcher = MATCHER_CONFIG.get_matcher()
+
+    none_and_bool = {"False", "None", "True"}
+
+    desc_doc = nlp.make_doc(" ".join(description.split()))
+    type_doc = nlp.make_doc(type_string)
+
+    desc_matches = matcher(desc_doc)
+    desc_matches = _nlp_matches_to_readable_matches(desc_matches, nlp, desc_doc)
+
+    type_matches = matcher(type_doc)
+    type_matches = _nlp_matches_to_readable_matches(type_matches, nlp, type_doc)
+
+    extracted_set = set(_extracted)
+
+    if any(x[0] == "ENUM_STR" for x in type_matches) and not extracted_set.difference(none_and_bool):
+        extracted_set.add("unlistable_str")
+
+    return extracted_set
+
+MATCHER_CONFIG = MatcherConfiguration()
+
+
+
--- tests/library_analyzer/processing/api/test_extract_valid_literals.py
+++ tests/library_analyzer/processing/api/test_extract_valid_literals.py
@@ -1,5 +1,5 @@
 import pytest
-from library_analyzer.processing.api._extract_valid_values import extract_valid_literals
+from library_analyzer.processing.api._extract_valid_values import extract_valid_literals, extract
 
 
 @pytest.mark.parametrize(
@@ -7,46 +7,72 @@ from library_analyzer.processing.api._extract_valid_values import extract_valid_
     [
         (
             "str",
-            'If "mean", then replace missing values using the mean along each column\nIf "median", then replace missing values using the median along each column\nIf "most_frequent", then replace missing using the most frequent value along each column\nIf "constant", then replace missing values with fill_value\n',
+            'If "mean", then replace missing values using the mean along each column '
+            'If "median", then replace missing values using the median along each column. '
+            'If "most_frequent", then replace missing using the most frequent value along each column. '
+            'If "constant", then replace missing values with fill_value.',
             ['"mean"', '"median"', '"most_frequent"', '"constant"'],
         ),
         (
             "str",
-            "If 'mean', then replace missing values using the mean along each column\nIf 'median', then replace missing values using the median along each column\nIf 'most_frequent', then replace missing using the most frequent value along each column\nIf 'constant', then replace missing values with fill_value\n",
-            ["'median'", "'most_frequent'", "'constant'", "'mean'"],
+            "If 'mean', then replace missing values using the mean along each column."
+            "If 'median', then replace "
+            "missing values using the median along each column. If 'most_frequent', then replace missing using the "
+            "most frequent value along each column. If 'constant', then replace missing values with fill_value.",
+            ['"median"', '"most_frequent"', '"constant"', '"mean"'],
         ),
         (
             "str, list or tuple of str",
-            'Attribute name(s) given as string or a list/tuple of strings Eg.: ["coef_", "estimator_", ...], "coef_"\n\nIf None, estimator is considered fitted if there exist an attribute that ends with a underscore and does not start with double underscore.',
+            'Attribute name(s) given as string or a list/tuple of strings Eg.: ["coef_", "estimator_", ...], "coef_"'
+            ' If None, estimator is considered fitted if there exist an attribute that ends with a '
+            'underscore and does not start with double underscore.',
             ["None", "unlistable_str"],
         ),
         (
             "bool or 'allow-nan'",
-            "Whether to raise an error on np.inf, np.nan, pd.NA in X. This parameter does not influence whether y can have np.inf, np.nan, pd.NA values. The possibilities are:\n\n\tTrue: Force all values of X to be finite.\n\tFalse: accepts np.inf, np.nan, pd.NA in X.\n\t'allow-nan': accepts only np.nan or pd.NA values in X. Values cannot be infinite.\n\n.. versionadded: 0.20 force_all_finite accepts the string 'allow-nan'.\n\n.. versionchanged: 0.23 Accepts pd.NA and converts it into np.nan",
-            ["'allow-nan'", "False", "True"],
+            "Whether to raise an error on np.inf, np.nan, pd.NA in X. This parameter does not influence whether y can "
+            "have np.inf, np.nan, pd.NA values. The possibilities are: "
+            "\n\n\tTrue: Force all values of X to be finite. "
+            "\n\tFalse: accepts np.inf, np.nan, pd.NA in X. "
+            "\n\t'allow-nan': accepts only np.nan or pd.NA values in X. Values cannot be infinite. "
+            "\n\n.. versionadded: 0.20 force_all_finite accepts the string 'allow-nan'. "
+            "\n\n.. versionchanged: 0.23 Accepts pd.NA and converts it into np.nan",
+            ['"allow-nan"', "False", "True"],
         ),
         (
             '{"random", "best"}',
-            'The strategy used to choose the split at each node. Supported strategies are "best" to choose the best split and "random" to choose the best random split.',
+            'The strategy used to choose the split at each node. Supported strategies are "best" to choose the best '
+            'split and "random" to choose the best random split.',
             ['"best"', '"random"'],
         ),
         (
             "bool or str",
-            "When set to True, change the display of 'values' and/or 'samples' to be proportions and percentages respectively.",
+            "When set to True, change the display of 'values' and/or 'samples' to be proportions and percentages "
+            "respectively.",
             ["False", "True", "unlistable_str"],
         ),
         (
             "int, RandomState instance or None",
-            'Controls the randomness of the estimator. The features are always randomly permuted at each split, even if splitter is set to "best". When max_features < n_features, the algorithm will select max_features at random at each split before finding the best split among them. But the best found split may vary across different runs, even if max_features=n_features. That is the case, if the improvement of the criterion is identical for several splits and one split has to be selected at random. To obtain a deterministic behaviour during fitting, random_state has to be fixed to an integer. See :term:Glossary <random_state> for details.',
+            'Controls the randomness of the estimator. The features are always randomly permuted at each split, '
+            'even if splitter is set to "best". When max_features < n_features, the algorithm will select '
+            'max_features at random at each split before finding the best split among them. But the best found split '
+            'may vary across different runs, even if max_features=n_features. That is the case, if the improvement of '
+            'the criterion is identical for several splits and one split has to be selected at random. To obtain a '
+            'deterministic behaviour during fitting, random_state has to be fixed to an integer. See :term:Glossary '
+            '<random_state> for details.',
             [],
         ),
         ("float", "Independent term in kernel function. It is only significant in 'poly' and 'sigmoid'.", []),
         (
             "float",
-            'When self.fit_intercept is True, instance vector x becomes [x, self.intercept_scaling], i.e. a "synthetic" feature with constant value equals to intercept_scaling is appended to the instance vector. The intercept becomes intercept_scaling * synthetic feature weight Note! the synthetic feature weight is subject to l1/l2 regularization as all other features. To lessen the effect of regularization on synthetic feature weight (and therefore on the intercept) intercept_scaling has to be increased.',
+            'When self.fit_intercept is True, instance vector x becomes [x, self.intercept_scaling], '
+            'i.e. a "synthetic" feature with constant value equals to intercept_scaling is appended to the instance '
+            'vector. The intercept becomes intercept_scaling * synthetic feature weight Note! the synthetic feature '
+            'weight is subject to l1/l2 regularization as all other features. To lessen the effect of regularization '
+            'on synthetic feature weight (and therefore on the intercept) intercept_scaling has to be increased.',
             [],
         ),
     ],
 )
 def test_extract_values(type_: str, description: str, expected_literals: list) -> None:
-    assert extract_valid_literals(description, type_) == set(expected_literals)
+    assert extract(description, type_) == set(expected_literals)
