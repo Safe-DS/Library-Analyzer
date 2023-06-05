c2db888f402f0e6c433decc2027be88523f46daa
Lukas Radermacher <lukas-radermacher@gmx.de>
2023-06-05 23:52:45 +0200

    docs: added documentation for `resolve_references.py`

--- src/library_analyzer/processing/api/_resolve_references.py
+++ src/library_analyzer/processing/api/_resolve_references.py
@@ -17,6 +17,19 @@ class MemberAccess(Expression):
 
 @dataclass
 class ScopeNode:
+    """ Represents a node in the scope tree.
+
+    The scope tree is a tree that represents the scope of a module. It is used to determine the scope of a reference.
+    On the top level, there is a ScopeNode for the module. Each ScopeNode has a list of children, which are the nodes
+    that are defined in the scope of the node. Each ScopeNode also has a reference to its parent node.
+
+    Attributes:
+
+        node        is the node in the AST that defines the scope of the node.
+        children    is a list of ScopeNodes that are defined in the scope of the node, is None if the node is a leaf node.
+        parent      is the parent node in the scope tree, is None if the node is the root node.
+    """
+
     node: astroid.Module | astroid.FunctionDef | astroid.ClassDef | astroid.AssignName | astroid.AssignAttr | astroid.Attribute | astroid.Call | astroid.Import | astroid.ImportFrom | MemberAccess
     children: list[ScopeNode] | None = None
     parent: ScopeNode | None = None
@@ -24,27 +37,45 @@ class ScopeNode:
 
 @dataclass
 class ScopeFinder:
+    """
+    A ScopeFinder instance is used to find the scope of a reference.
+
+    The scope of a reference is the node in the scope tree that defines the reference.
+    It is determined by walking the AST and checking if the reference is defined in the scope of the current node.
+
+    Attributes:
+
+        current_node_stack      stack of nodes that are currently visited by the ASTWalker .
+        children:               All found children nodes are stored in children until their scope is determined.
+    """
+
     current_node_stack: list[ScopeNode] = field(default_factory=list)
     children: list[ScopeNode] = field(default_factory=list)
 
     def detect_scope(self, node: astroid.NodeNG) -> None:
+        """
+        Detect the scope of the given node.
+
+        Detecting the scope of a node means finding the node in the scope tree that defines the scope of the given node.
+        The scope of a node is defined by the parent node in the scope tree.
+        """
+
         current_scope = node
         outer_scope_children: list[ScopeNode] = []
         inner_scope_children: list[ScopeNode] = []
         for child in self.children:
-            if child.parent is not None and child.parent.node != current_scope:
-                outer_scope_children.append(child)  # select all children from the outer scope
+            if child.parent is not None and child.parent.node != current_scope:  # check if the child is in the scope of the current node
+                outer_scope_children.append(child)  # add the child to the outer scope
             else:
-                inner_scope_children.append(child)  # select all children from this scope
+                inner_scope_children.append(child)  # add the child to the inner scope
 
-        self.current_node_stack[-1].children = inner_scope_children  # add all children from this scope to the parent
-        self.children = outer_scope_children
-        self.children.append(self.current_node_stack[-1])
-        self.current_node_stack.pop()
-
+        self.current_node_stack[-1].children = inner_scope_children  # set the children of the current node
+        self.children = outer_scope_children  # keep the children that are not in the scope of the current node
+        self.children.append(self.current_node_stack[-1])  # add the current node to the children
+        self.current_node_stack.pop()  # remove the current node from the stack
 
     def enter_module(self, node: astroid.Module) -> None:
-        self.current_node_stack.append(ScopeNode(node=node, children=None, parent=None))
+        self.current_node_stack.append(ScopeNode(node=node, children=None, parent=None))  # add a module node to the stack, the module node is the root node, so it has no parent
 
     def leave_module(self, node: astroid.Module) -> None:
         self.detect_scope(node)
@@ -69,7 +100,7 @@ class ScopeFinder:
     def enter_assignname(self, node: astroid.AssignName) -> None:
         if isinstance(node.parent, astroid.Arguments):
             if node.name == "self":
-                pass
+                pass  # TODO: Special treatment for self parameter
             else:
                 parent = self.current_node_stack[-1]
                 scope_node = ScopeNode(node=node, children=None, parent=parent)
@@ -110,8 +141,8 @@ def get_scope(code: str) -> list[ScopeNode]:
     module = astroid.parse(code)
     walker.walk(module)
 
-    scopes = scope_handler.children
-    scope_handler.children = []
-    scope_handler.current_node_stack = []
+    scopes = scope_handler.children  # get the children of the root node, which are the scopes of the module
+    scope_handler.children = []  # reset the children
+    scope_handler.current_node_stack = []  # reset the stack
 
     return scopes
