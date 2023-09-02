98b5438cc8011db66a988a5546288a89334dc6fb
nvollroth <n.vollroth@live.de>
2023-09-02 10:09:24 +0200

    Added a test case

--- tests/library_analyzer/processing/api/test_extract_valid_literals.py
+++ tests/library_analyzer/processing/api/test_extract_valid_literals.py
@@ -45,6 +45,7 @@ from library_analyzer.processing.api._extract_valid_values import extract_valid_
         ("str, float", "If `False`, the value must be between 0 and 1.", ["unlistable_str"]),
         ("str, float", "If '123*mean', the value must be between 0 and 1.", ["unlistable_str"]),
         ("'auto' or float", "If float, the value must be between 0 and 1.", ['"auto"']),
+        ("str or bool", "Valid values are 'auto', 'allow-nan', False.", ['"auto"', '"allow-nan"', "False", "True"]),
         (
             "str, callable",
             (
