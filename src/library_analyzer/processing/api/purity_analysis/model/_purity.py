682f86f93841f0ebb0161a36e22bc78fd75bee96
lukarade <84092952+lukarade@users.noreply.github.com>
2023-10-12 02:43:11 +0200

    feat: adapted Purity data model to fit our new definitions

--- src/library_analyzer/processing/api/purity_analysis/model/_purity.py
+++ src/library_analyzer/processing/api/purity_analysis/model/_purity.py
@@ -9,6 +9,87 @@ import astroid
 from library_analyzer.processing.api.purity_analysis.model._scope import NodeID
 
 
+@dataclass
+class PurityResult(ABC):
+    """Class for purity results."""
+    reasons: list[ImpurityReason]
+
+
+@dataclass
+class Pure(PurityResult):
+    """Class for pure results.
+
+    A function is pure if it has no (External-, Internal-)Read nor (External-, Internal-)Write side effects.
+    A pure function must also have no unknown reasons.
+    """
+    reasons: list = field(default_factory=list)
+
+
+@dataclass
+class Impure(PurityResult):
+    """Class for impure results.
+
+    A function is impure if it has at least one (External-, Internal-)Read OR (External-, Internal-)Write side effect.
+    An impure function must also have no unknown reasons.
+
+    Be aware that a function can be impure because of multiple reasons.
+    Also, Impure != Pure since: not Pure would mean a function is either unknown or has at least one
+    (External-, Internal-)Read (External-, Internal-) or Write side effect.
+    """
+    reasons: list[ImpurityReason]
+
+
+@dataclass
+class ImpurityReason(ABC):
+    """Class for impurity reasons.
+
+    If a funtion is impure it is because of one or more impurity reasons.
+    """
+    pass
+
+
+@dataclass
+class InternalRead(ImpurityReason):  # VariableRead
+    """Class for internal variable reads (GlobalVariable / global Fields)."""
+    source: Expression
+
+
+@dataclass
+class ExternalRead(ImpurityReason):  # FileRead
+    """Class for external variable reads (File / Database)."""
+    source: Expression
+
+
+@dataclass
+class InternalWrite(ImpurityReason):  # VariableWrite
+    """Class for internal variable writes (GlobalVariable / global Fields)."""
+    expression: Expression
+
+
+@dataclass
+class ExternalWrite(ImpurityReason):  # FileWrite
+    """Class for external variable writes (File / Database)."""
+    source: Expression
+
+
+@dataclass
+class Call(ImpurityReason):
+    """Class for impure function calls."""
+    expression: Expression
+
+
+@dataclass
+class SystemInteraction(ImpurityReason):
+    """Class for system interactions (e.g. print)."""
+    kind = str  # SystemIn / SystemOut / SystemErr
+
+
+@dataclass
+class Unknown(ImpurityReason):
+    """Class for unknown impurity reasons."""
+    kind = str
+
+
 # Type of access
 class Expression(astroid.NodeNG, ABC):
     # @abstractmethod
@@ -23,8 +104,6 @@ class AttributeAccess(Expression):
 
     name: str
 
-    # def __hash__(self) -> int:
-
 
 @dataclass
 class GlobalAccess(Expression):
@@ -32,8 +111,6 @@ class GlobalAccess(Expression):
 
     name: str
     module: str = "None"
-
-    # def __hash__(self) -> int:
 
 
 @dataclass
@@ -43,181 +120,21 @@ class ParameterAccess(Expression):
     name: str
     function: str
 
-    # def __hash__(self) -> int:
-
 
 @dataclass
 class InstanceAccess(Expression):
     """Class for field access of an instance attribute (receiver.target)."""
-
     receiver: Expression
     target: Expression
 
-    # def __hash__(self) -> int:
 
 
 @dataclass
 class StringLiteral(Expression):
     value: str
 
-    # def __hash__(self) -> int:
-
 
 @dataclass
 class Reference(Expression):
     name: str
-
-    # def __hash__(self) -> int:
-
-
-class ImpurityCertainty(Enum):
-    DEFINITELY_PURE = auto()
-    MAYBE_IMPURE = auto()
-    DEFINITELY_IMPURE = auto()
-
-
-# Reasons for impurity
-class ImpurityIndicator(ABC):
-    certainty: ImpurityCertainty
-
-    # @abstractmethod
-    # def __hash__(self) -> int:
-    #     pass
-
-    @abstractmethod
-    def is_side_effect(self) -> bool:
-        pass
-
-
-@dataclass
-class ConcreteImpurityIndicator(ImpurityIndicator):
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return False
-
-
-@dataclass
-class VariableRead(ImpurityIndicator):
-    expression: Expression
-    certainty = ImpurityCertainty.MAYBE_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return False
-
-
-@dataclass
-class VariableWrite(ImpurityIndicator):
-    expression: Expression
-    certainty = ImpurityCertainty.MAYBE_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return True
-
-
-@dataclass
-class FileRead(ImpurityIndicator):
-    source: Expression
-    certainty = ImpurityCertainty.DEFINITELY_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return False
-
-
-@dataclass
-class FileWrite(ImpurityIndicator):
-    source: Expression
-    certainty = ImpurityCertainty.DEFINITELY_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return True
-
-
-@dataclass
-class UnknownCallTarget(ImpurityIndicator):
-    expression: Expression
-    certainty = ImpurityCertainty.DEFINITELY_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return True  # TODO: improve this to make analysis more precise
-
-
-@dataclass
-class Call(ImpurityIndicator):
-    expression: Expression
-    certainty = ImpurityCertainty.DEFINITELY_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return True  # TODO: improve this to make analysis more precise
-
-
-@dataclass
-class SystemInteraction(ImpurityIndicator):
-    certainty = ImpurityCertainty.DEFINITELY_IMPURE
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return True
-
-
-@dataclass
-class BuiltInFunction(ImpurityIndicator):
-    """Class for built-in functions."""
-
-    expression: Expression
-    indicator: ImpurityIndicator  # this should be a list to handle multiple reasons
-    certainty: ImpurityCertainty
-
-    # def __hash__(self) -> int:
-
-    def is_side_effect(self) -> bool:
-        return False
-
-
-class PurityResult(ABC):  # noqa: B024
-    def __init__(self) -> None:
-        self.reasons: list[ImpurityIndicator] = []
-
-
-@dataclass
-class DefinitelyPure(PurityResult):
-    reasons: list = field(default_factory=list)
-
-
-@dataclass
-class MaybeImpure(PurityResult):
-    reasons: list[ImpurityIndicator]
-
-    # def __hash__(self) -> int:
-
-
-@dataclass
-class DefinitelyImpure(PurityResult):
-    reasons: list[ImpurityIndicator]
-
-    # def __hash__(self) -> int:
-
-
-@dataclass
-class PurityInformation:
-    id: NodeID
-    reasons: list[ImpurityIndicator]
-
-    # def __hash__(self) -> int:
-
-    # def __eq__(self, other: object) -> bool:
-    #     if not isinstance(other, PurityInformation):
 
--- tests/library_analyzer/processing/api/__init__.py
+++ tests/library_analyzer/processing/api/__init__.py
@@ -1,3 +1,4 @@
-from tests.library_analyzer.processing.api.test_get_module_data import transform_member_access
+from tests.library_analyzer.processing.api.test_get_module_data import transform_member_access, SimpleFunctionReference
 
-__all__ = ["transform_member_access"]
+__all__ = ["transform_member_access",
+           "SimpleFunctionReference"]
