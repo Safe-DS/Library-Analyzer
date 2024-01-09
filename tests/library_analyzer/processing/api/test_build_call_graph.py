2db3efb9128eb94f221b1c58f61f28b9e0bc95e0
lukarade <84092952+lukarade@users.noreply.github.com>
2024-01-09 02:13:02 +0100

    test: added member access test for built_call_graph

--- tests/library_analyzer/processing/api/test_build_call_graph.py
+++ tests/library_analyzer/processing/api/test_build_call_graph.py
@@ -259,31 +259,182 @@ double = lambda x: 2 * x
                 ".double.2.9": set(),
             },
         ),
-        (  # language=Python "function call of functions with same name"
+
+    ],
+    ids=[
+        "function call - in declaration order",
+        "function call - against declaration flow",
+        "function call - against declaration flow with multiple calls",
+        "function conditional with branching",
+        "function call with cycle - direct entry",
+        "function call with cycle - one entry point",
+        "function call with cycle - many entry points",
+        "function call with cycle - other call in cycle",
+        "function call with cycle - multiple other calls in cycle",
+        # "function call with cycle - cycle within a cycle",
+        "recursive function call",
+        "builtin function call",
+        "external function call",
+        "lambda call",
+    ],
+)
+def test_build_call_graph(code: str, expected: dict[str, set]) -> None:
+    module_data = get_module_data(code)
+    call_graph_forest = build_call_graph(module_data.functions, module_data.function_references)
+
+    transformed_call_graph_forest: dict = {}
+    for tree_id, tree in call_graph_forest.graphs.items():
+        transformed_call_graph_forest[f"{tree_id}"] = set()
+        for child in tree.children:
+            transformed_call_graph_forest[f"{tree_id}"].add(child.data.symbol.id.__str__())
+
+    assert transformed_call_graph_forest == expected
+
+
+@pytest.mark.parametrize(
+    ("code", "expected"),
+    [
+        (  # language=Python "class call - init",
+            """
+class A:
+    pass
+
+def fun():
+    a = A()
+
+            """,  # language=none
+            {
+                ".fun.5.0": {".A.2.0"},
+            },
+        ),
+        (  # language=Python "member access - class",
+            """
+class A:
+    class_attr1 = 20
+
+def fun():
+    a = A().class_attr1
+
+            """,  # language=none
+            {
+                ".A.2.0": set(),
+                ".fun.5.0": {".A.2.0"},
+            },
+        ),
+        (  # language=Python "member access - class without init",
+            """
+class A:
+    class_attr1 = 20
+
+def fun():
+    a = A.class_attr1
+
+            """,  # language=none
+            {
+                ".A.2.0": set(),
+                ".fun.5.0": {".A.2.0"},  # TODO: LARS do we want this?
+            },
+        ),
+        (  # language=Python "member access - methode",
+            """
+class A:
+    class_attr1 = 20
+
+    def g(self):
+        pass
+
+def fun1():
+    a = A()
+    a.g()
+
+def fun2():
+    a = A().g()
+
+            """,  # language=none
+            {
+                ".A.2.0": set(),
+                ".g.5.4": set(),
+                ".fun1.8.0": {".A.2.0", ".g.5.4"},
+                ".fun2.12.0": {".A.2.0", ".g.5.4"},
+            },
+        ),
+        (  # language=Python "member access - init",
+            """
+class A:
+    def __init__(self):
+        pass
+
+def fun():
+    a = A()
+
+            """,  # language=none
+            {
+                ".A.2.0": {".__init__.3.4"},
+                ".__init__.3.4": set(),
+                ".fun.6.0": {".A.2.0"},
+            },
+        ),
+        (  # language=Python "member access - instance function",
+            """
+class A:
+    def __init__(self):
+        self.a_inst = B()
+
+class B:
+    def __init__(self):
+        pass
+
+    def b_fun(self):
+        pass
+
+def fun1():
+    a = A()
+    a.a_inst.b_fun()
+
+def fun2():
+    a = A().a_inst.b_fun()
+
+            """,  # language=none
+            {
+                ".A.2.0": {".__init__.3.4"},
+                ".__init__.3.4": {".B.6.0"},
+                ".B.6.0": {".__init__.7.4"},
+                ".__init__.7.4": set(),
+                ".b_fun.10.4": set(),
+                ".fun1.13.0": {".A.2.0", ".b_fun.10.4"},
+                ".fun2.17.0": {".A.2.0", ".b_fun.10.4"},
+            },
+        ),
+        (  # language=Python "member access - function call of functions with same name"
             """
 class A:
     @staticmethod
-    def fun():
-        return "Function A"
+    def add(a, b):
+        return a + b
 
 class B:
     @staticmethod
-    def fun():
-        return "Function B"
+    def add(a, b):
+        return a + 2 * b
 
-def call_function(a):
-    if a == 1:
-        return A.fun()
-    else:
-        return B.fun()
+def fun_a():
+    x = A()
+    x.add(1, 2)
+
+def fun_b():
+    x = B()
+    x.add(1, 2)
             """,  # language=none
             {
-                ".fun.4.4": set(),
-                ".fun.9.4": set(),
-                ".call_function.12.0": {".fun.4.4", ".fun.9.4"},
+                ".A.2.0": set(),
+                ".B.7.0": set(),
+                ".add.4.4": set(),
+                ".add.10.4": set(),
+                ".fun_a.13.0": {".A.2.0", ".add.4.4"},  # TODO: is it possible to distinguish between the two add functions?
+                ".fun_b.18.0": {".B.7.0", ".add.10.4"},
             },
         ),
-        (  # language=Python "function call of functions with same name and nested calls",
+        (  # language=Python "member access - function call of functions with same name and nested calls",
             """
 def fun1():
     pass
@@ -302,38 +453,83 @@ class B:
     def add(a, b):
         fun2()
         return a + 2 * b
-
-x = A()
-x.add(1, 2)
             """,  # language=none
             {
+                ".A.8.0": set(),
+                ".B.14.0": set(),
                 ".fun1.2.0": set(),
                 ".fun2.5.0": {".print.6.4"},  # print is a builtin function and therefore has no function def to reference -> we use the id of the call node for simplicity
                 ".add.10.4": {".fun1.2.0"},
                 ".add.16.4": {".fun2.5.0"},
             },
         ),
+        (  # language=Python "member access - function call of functions with same name (no distinction possible)"
+            """
+class A:
+    @staticmethod
+    def fun():
+        return "Function A"
+
+class B:
+    @staticmethod
+    def fun():
+        return "Function B"
+
+def fun_out(a):
+    if a == 1:
+        x = A()
+    else:
+        x = B()
+    x.fun()
+            """,  # language=none
+            {
+                ".A.2.0": set(),
+                ".B.8.0": set(),
+                ".fun.4.4": set(),
+                ".fun.9.4": set(),
+                ".fun_out.12.0": {".A.2.0", ".B.8.0", ".fun.4.4", ".fun.9.4"},   # here we cannot distinguish between the two fun functions
+            },
+        ),
+        (  # language=Python "member access - function call of functions with same name (different signatures)"
+            """
+class A:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+class B:
+    @staticmethod
+    def add(a, b, c):
+        return a + b + c
+
+def fun():
+    A.add(1, 2)
+    B.add(1, 2, 3)
+            """,  # language=none
+            {
+                ".A.2.0": set(),
+                ".B.7.0": set(),
+                ".add.4.4": set(),
+                ".add.9.4": set(),
+                ".fun.12.0": {".A.2.0", ".B.7.0", ".add.4.4", ".add.9.4"},   # TODO: here we can distinguish between the two add functions because of their signature
+            },
+        ),
+
     ],
     ids=[
-        "function call - in declaration order",
-        "function call - against declaration flow",
-        "function call - against declaration flow with multiple calls",
-        "function conditional with branching",
-        "function call with cycle - direct entry",
-        "function call with cycle - one entry point",
-        "function call with cycle - many entry points",
-        "function call with cycle - other call in cycle",
-        "function call with cycle - multiple other calls in cycle",
-        # "function call with cycle - cycle within a cycle",
-        "recursive function call",
-        "builtin function call",
-        "external function call",
-        "lambda call",
-        "function call of functions with same name",
-        "function call of functions with same name and nested calls",
-    ],  # TODO: LARS how do we build a call graph for a.b.c.d()?
+        "class call - init",
+        "member access - class",
+        "member access - class without init",
+        "member access - methode",
+        "member access - init",
+        "member access - instance function",
+        "member access - function call of functions with same name",
+        "member access - function call of functions with same name and nested calls",
+        "member access - function call of functions with same name (no distinction possible)",
+        "member access - function call of functions with same name (different signatures)",
+    ],
 )
-def test_build_call_graph(code: str, expected: dict[str, set]) -> None:
+def test_build_call_graph_member_access(code: str, expected: dict[str, set]) -> None:
     module_data = get_module_data(code)
     call_graph_forest = build_call_graph(module_data.functions, module_data.function_references)
 
