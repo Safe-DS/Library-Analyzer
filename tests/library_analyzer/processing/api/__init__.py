604b4caaabf4e97ba0bac97425a698a32f127da9
lukarade <84092952+lukarade@users.noreply.github.com>
2024-01-19 13:47:17 +0100

    fix: added separate test folder for purity analysis

--- tests/library_analyzer/processing/api/__init__.py
+++ tests/library_analyzer/processing/api/__init__.py
@@ -1,3 +1,1 @@
-from tests.library_analyzer.processing.api.test_get_module_data import SimpleFunctionReference, transform_member_access
 
-__all__ = ["transform_member_access", "SimpleFunctionReference"]
---
+++ tests/library_analyzer/processing/api/purity_analysis/__init__.py
@@ -0,0 +1,7 @@
+from tests.library_analyzer.processing.api.purity_analysis.test_get_module_data import (
+    SimpleFunctionReference,
+    transform_member_access,
+)
+
+__all__ = ["transform_member_access", "SimpleFunctionReference"]
+
