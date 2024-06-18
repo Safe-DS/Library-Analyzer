7069e07128b337674d1544bb9203befb6ea4abd7
lukarade <84092952+lukarade@users.noreply.github.com>
2024-06-18 19:35:59 +0200

    feat: activated short mode as default (only the reason count will be listed in the result file)

--- src/library_analyzer/cli/_run_api.py
+++ src/library_analyzer/cli/_run_api.py
@@ -36,4 +36,4 @@ def _run_api_command(
 
     api_purity = get_purity_results(src_dir_path)
     out_file_api_purity = out_dir_path.joinpath(f"{package}__api_purity.json")
-    api_purity.to_json_file(out_file_api_purity)
+    api_purity.to_json_file(out_file_api_purity, True)  # True shortens the results to only contain the count of each reason.
